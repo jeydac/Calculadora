c25454870ce046eb1691914acfc2435f52b44d8c
Jessica <jessica.d.costa@hotmail.com>
2025-07-23 09:26:21 +0100

    primeiro comentario

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+## Primeiro comentário
+
 import os
 import time
 
@@ -32,3 +34,14 @@ if __name__ == "__main__":
             time.sleep(2)
 
     print('\nVolte sempre!\n')
+    
+    
+    
+    """ 
+4º Efetuar as alterações necessárias fazendo vários commits e push
+5º Têm de terminar a implementação de uma calculadora
+No bloco principal deverão implementar uma forma de solicitar 2 numeros ao utilizador e o tipo de operação a realizar.
+Após exibição do resultado deverão perguntar se o utlizador deseja continuar a efetuar mais operações
+As operações a implementar serão a soma, subtração, divisão, multiplicação e exponenciação
+ 
+6º Ir ao github verificar as alterações e partilhar o link do repositório na tarefa"""
